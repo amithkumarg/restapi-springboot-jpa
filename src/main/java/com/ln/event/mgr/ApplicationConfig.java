0598ff3657060d497ef8eaefeaa1ac1d01673984
Amith Kumar <amith_kumarg@yahoo.co.in>
2018-05-14 11:41:05 -0700

    Removed unnecessary main run from Config class, typo error

--- src/main/java/com/ln/event/mgr/ApplicationConfig.java
+++ src/main/java/com/ln/event/mgr/ApplicationConfig.java
@@ -1,6 +1,5 @@
 package com.ln.event.mgr;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
@@ -16,9 +15,5 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @EntityScan(basePackages ={ "com.ln.event.mgr.model"})
 @EnableTransactionManagement
 public class ApplicationConfig {
-	
-	public static void main(String[] args){
-		SpringApplication.run(ApplicationConfig.class, args);
-	}
-
+  
 }
